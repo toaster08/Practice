b586898a898bf899fc7db747cdb17bccff72d472
toaster08 <y10star.hon@gmail.com>
2021-04-20 02:24:35 +0900

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by 山田　天星 on 2021/04/20.
+//  Copyright © 2021 toaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
